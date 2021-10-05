7cca688b03c194fcb548776f10a96f4ea0ccba82
tabu guduru <tabuguduru@gmail.com>
2021-10-05 16:40:02 +0200

    update

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-/* You will to add includes here */
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <math.h>
 
-// Enable if you want debugging to be printed, see examble below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
+
 #define DEBUG
 
 
@@ -13,21 +18,116 @@
 
 int main(int argc, char *argv[]){
 
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
-  char delim[]=":";
-  char *Desthost=strtok(argv[1],delim);
-  char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
 
-  /* Do magic */
-  int port=atoi(Destport);
-#ifdef DEBUG 
-  printf("Host %s, and port %d.\n",Desthost,port);
-#endif
+	int sock, port;
+	char delim[]=":";
+	char *Desthost=strtok(argv[1],delim);
+	char *Destport=strtok(NULL,delim);
+	// *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+	// *Dstport points to whatever string came after the delimiter. 
+
+	port=atoi(Destport);
+	#ifdef DEBUG 
+		printf("Host %s, and port %d.\n",Desthost,port);
+	#endif
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		printf("Cannot create socket");
+		return 0;
+	}
+
+	struct sockaddr_in client;
+	client.sin_family = AF_INET;
+	client.sin_port = htons(port);
+	client.sin_addr.s_addr = inet_addr(Desthost);
+
+	int request = connect(sock, (struct sockaddr *)  &client, sizeof(client));
+	if (request < 0) {
+		return 0;
+	}
+
+	char buffer[1024], srv_msg[1024];
+	char *message;
+	message = "OK\n";
+	if (recv(sock, &buffer, sizeof(buffer), 0) < 0) {
+		return 0;
+	}
+	printf("%s", buffer);
+	if (strcmp(buffer, "TEXT TCP 1.0\n\n") == 0) {
+		if (send(sock, message, strlen(message), 0) < 0) {
+			return 0;
+		}
+	}
+	bzero(srv_msg, sizeof(srv_msg));
+	if (recv(sock, &srv_msg, sizeof(srv_msg), 0) < 0) {
+		return 0;
+	}
+	printf("recieved operation and operands are : %s", srv_msg);
+	int j = 0;
+	char *com[3];
+	com[j] = strtok(srv_msg, " ");
+	while (com[j] != NULL)
+	{
+		com[++j] = strtok(NULL, " ");
+	}
+	int i1, i2, iresult;
+	float f1, f2, fresult;
+
+	char command[10];
+	strcpy(command, com[0]);
+	char result[256];
+	if (command[0] == 'f') {
+		f1 = atof(com[1]);
+		f2 = atof(com[2]);
+		if (strcmp(command, "fadd") == 0) {
+			fresult = f1 + f2;
+		}
+		else if (strcmp(command, "fmul") == 0) {
+			fresult = f1 * f2;
+		}
+		else if (strcmp(command, "fsub") == 0) {
+			fresult = f1 - f2;
+		}
+		else if (strcmp(command, "fdiv") == 0) {
+			fresult = f1 / f2;
+		}
+		printf("%s %8.8g %8.8g = %8.8g\n", command, f1, f2, fresult);
+		sprintf(result, "%8.8g\n", fresult);
+		if (send(sock, result, strlen(result), 0) < 0) {
+			return 0;
+		}
+
+	}
+	else {
+		i1 = atoi(com[1]);
+		i2 = atoi(com[2]);
+		if (strcmp(command, "add") == 0) {
+			iresult = i1 + i2;
+		}
+		else if (strcmp(command, "sub") == 0) {
+			iresult = i1 - i2;
+		}
+		else if (strcmp(command, "div") == 0) {
+			iresult = i1 / i2;
+		}
+		else if (strcmp(command, "mul") == 0) {
+			iresult = i1 * i2;
+		}
+		printf("%s %d %d = %d\n", command, i1, i2, iresult);
+		sprintf(result, "%d\n", iresult);
+		if (send(sock, result, strlen(result), 0) < 0) {
+			return 0;
+		}
+	}
+	char reply[256];
+	if (recv(sock, &reply, sizeof(reply), 0) < 0) {
+		return 0;
+	}
+	printf("\n server reply: %s\n", reply);
+	close(sock);
+
+	return 0;
+
 
   
 }
--- servermain.cpp
+++ servermain.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/* You will to add includes here */
-
-
-// Included to get the support library
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "calcLib.c"
 #include <calcLib.h>
 
 // Enable if you want debugging to be printed, see examble below.
@@ -17,21 +19,135 @@ using namespace std;
 
 int main(int argc, char *argv[]){
   
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
-  char delim[]=":";
-  char *Desthost=strtok(argv[1],delim);
-  char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
-
-  /* Do magic */
-  int port=atoi(Destport);
+	int sock,request,port;
+	char delim[]=":";
+	char *Desthost=strtok(argv[1],delim);
+	char *Destport=strtok(NULL,delim);
+	// *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+	// *Dstport points to whatever string came after the delimiter. 
+	port=atoi(Destport);
 #ifdef DEBUG  
-  printf("Host %s, and port %d.\n",Desthost,port);
+	printf("Host %s, and port %d.\n",Desthost,port);
 #endif
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = inet_addr(Desthost);
+
+	request = bind(sock, (struct sockaddr *) &server, sizeof(server));
+	if (request < 0) {
+		return 0;
+	}
+
+	listen(sock, 10);
+	int cnctn;
+	initCalcLib();
+	while (1) {
+		socklen_t cli_len;
+		cli_len = sizeof(struct sockaddr_in);
+		cnctn = accept(sock, (struct sockaddr *) &server, &cli_len);
+		char rcv_msg[256];
+		bzero(rcv_msg, sizeof(rcv_msg));
+		strcpy(rcv_msg, "TEXT TCP 1.0\n\n");
+		if (send(cnctn, rcv_msg, strlen(rcv_msg), 0) < 0) {
+			return 0;
+		}
+		bzero(rcv_msg, sizeof(rcv_msg));
+		if (recv(cnctn, &rcv_msg, sizeof(rcv_msg), 0) < 0) {
+			return 0;
+		}
+
+		char *ptr;
+		ptr = randomType();
+
+		double f1, f2, fresult;
+		int i1, i2, iresult;
+		char reply[256];
+		char srv_result[256];
+		char client_reply[256];
+		float result, D;
+
+		if (ptr[0] == 'f') {
+
+			f1 = randomFloat();
+			f2 = randomFloat();
+			bzero(reply, sizeof(reply));
+			sprintf(reply, "%s %8.8g %8.8g\n", ptr, f1, f2);
+			if (strcmp(ptr, "fadd") == 0) {
+				fresult = f1 + f2;
+			}
+			else if (strcmp(ptr, "fsub") == 0) {
+				fresult = f1 - f2;
+			}
+			else if (strcmp(ptr, "fmul") == 0) {
+				fresult = f1 * f2;
+			}
+			else if (strcmp(ptr, "fdiv") == 0) {
+				fresult = f1 / f2;
+			}
+			sprintf(srv_result, "%8.8g\n", fresult);
+			if (send(cnctn, reply, strlen(reply), 0) < 0) {
+				return 0;
+			}
+			if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
+				return 0;
+			}
+			result = atof(client_reply);
+			D = abs(result - fresult);
+
+		}
+		else {
+
+			i1 = randomInt();
+			i2 = randomInt();
+			bzero(reply, sizeof(reply));
+			sprintf(reply, "%s %d %d\n", ptr, i1, i2);
+
+			if (strcmp(ptr, "add") == 0) {
+				iresult = i1 + i2;
+			}
+			else if (strcmp(ptr, "mul") == 0) {
+				iresult = i1 * i2;
+			}
+			else if (strcmp(ptr, "sub") == 0) {
+				iresult = i1 - i2;
+			}
+			else if (strcmp(ptr, "div") == 0) {
+				iresult = i1 / i2;
+			}
+			sprintf(srv_result, "%d\n", iresult);
+			if (send(cnctn, reply, strlen(reply), 0) < 0) {
+				return 0;
+			}
+			if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
+				return 0;
+			}
+			float result, D;
+			result = atof(client_reply);
+			D = abs(result - iresult);
+		}
+
+		char *pass;
+		char *fail;
+		pass = "OK\n";
+		fail = "error\n";
+		if (D < 0.0001) {
+			if (send(cnctn, pass, sizeof(pass), 0) < 0) {
+				return 0;
+			}
+		}
+		else {
+			if (send(cnctn, fail, sizeof(fail), 0) < 0) {
+				return 0;
+			}
+		}
+		close(cnctn);
+	}
+
+	close(sock);
 
 
 }
