b1631801e47f2f098f452efa8051854779b0fda6
root <root@WCND14702L5>
2024-09-02 15:21:06 +0200

    Updated the clientmain.cpp to handle the DNS,ipv4,ipv6 Updated the server functionality to handle the IPV4/IPV6/DNS

--- clientmain.cpp
+++ clientmain.cpp
@@ -9,125 +9,244 @@
 #include <netdb.h>
 #include <math.h>
 
-
 #define DEBUG
 
-
-// Included to get the support library
 #include <calcLib.h>
 
-int main(int argc, char *argv[]){
+int check_desthost(char *Desthost) {
+    struct sockaddr_in sa; //struct for ipv4 
+    struct sockaddr_in6 ipv6_sa; //struct for ipv6
+    struct addrinfo hints, *res;  //struct for dns data
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; 
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (inet_pton(AF_INET, Desthost, &(sa.sin_addr)) == 1) {
+        return 1;
+    } else if (inet_pton(AF_INET6, Desthost, &(ipv6_sa.sin6_addr)) == 1) {
+        return 2;
+    } else if (getaddrinfo(Desthost, NULL, &hints, &res) == 0) {
+        freeaddrinfo(res);
+        return 3;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    char *input = argv[1];
+    char *port_no = strrchr(input, ':');
+    if (port_no == NULL) {
+        printf("Only accepted Input format host:port\n");
+        return 0;
+    }
+
+    *port_no = '\0'; // Replacing the colon with null terminator
+
+    int sock, port;
+    char *Desthost = input;
+    char *Destport = port_no + 1; //Get the value at the address after the port_no
+
+    int address_type = check_desthost(Desthost); //Check the address type 
+
+    if (address_type == 0) {  // Check if address type is 0
+        printf("Invalid IP address\n");
+        return 0;
+    }
+
+    port = atoi(Destport);
+    printf("Host %s, and port %d.\n", Desthost, port);
+#ifdef DEBUG
+    printf("Connected to %s:%d and local.\n", Desthost, port);
+#endif
+
+    if (address_type == 1) {
+        struct sockaddr_in client;
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            perror("Cannot create socket with IPv4 address");
+            return 0;
+        }
+
+        client.sin_family = AF_INET;
+        client.sin_port = htons(port);
+        client.sin_addr.s_addr = inet_addr(Desthost);
+
+        if (connect(sock, (struct sockaddr *) &client, sizeof(client)) < 0) {
+            perror("Connection failed");
+            close(sock);
+            return 0;
+        }
+    } else if (address_type == 2) {
+        struct sockaddr_in6 client;
+        memset(&client, 0, sizeof(client));
+
+        sock = socket(AF_INET6, SOCK_STREAM, 0);
+        if (sock < 0) {
+            perror("Cannot create socket with IPv6 address");
+            return 0;
+        }
+
+        client.sin6_family = AF_INET6;
+        client.sin6_port = htons(port);
+        if (inet_pton(AF_INET6, Desthost, &client.sin6_addr) != 1) {
+            perror("Invalid IPv6 address");
+            close(sock);
+            return 0;
+        }
+
+        if (connect(sock, (struct sockaddr *) &client, sizeof(client)) < 0) {
+            perror("Connection failed with the address");
+            close(sock);
+            return 0;
+        }
+    } else if (address_type == 3) {
+        struct addrinfo hints, *res, *rp;
+
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;    //  IPv4 or IPv6
+        hints.ai_socktype = SOCK_STREAM;
+
+        int status = getaddrinfo(Desthost, Destport, &hints, &res);
+        if (status != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+            return 0;
+        }
+
+        for (rp = res; rp != NULL; rp = rp->ai_next) {
+            sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+            if (sock < 0) {
+                continue;
+            }
+
+            if (connect(sock, rp->ai_addr, rp->ai_addrlen) == 0) {
+                printf("Connected to host %s\n", Desthost);
+                break;
+            }
+            close(sock);
+        }
+
+        if (rp == NULL) { 
+            fprintf(stderr, "Check your DNS name, unable to attach to any address\n");
+            freeaddrinfo(res);
+            return 0;
+        }
+        freeaddrinfo(res);
+    }
+
+    char buffer[1024], srv_msg[1024];
+    char *message = "OK\n";
+
+    // Receive the initial message
+    bzero(buffer, sizeof(buffer));
+    if (recv(sock, buffer, sizeof(buffer) - 1, 0) < 0) {
+        perror("Failed to receive initial message");
+        close(sock);
+        return 0;
+    }
+    buffer[sizeof(buffer) - 1] = '\0';
+    printf("%s", buffer);
+
+    if (strcmp(buffer, "TEXT TCP 1.0\n\n") == 0) {
+        if (send(sock, message, strlen(message), 0) < 0) {
+            perror("Failed to send OK message");
+            close(sock);
+            return 0;
+        }
+    } else {
+        printf("Server repsonse doesn match!: %s\n", buffer);
+        close(sock);
+        return 0;
+    }
+
+    // Receive the operation and operands
+    bzero(srv_msg, sizeof(srv_msg));
+    if (recv(sock, srv_msg, sizeof(srv_msg) - 1, 0) < 0) {
+        perror("Failed to receive operation message");
+        close(sock);
+        return 0;
+    }
+    srv_msg[sizeof(srv_msg) - 1] = '\0'; 
 
 
-	int sock, port;
-	char delim[]=":";
-	char *Desthost=strtok(argv[1],delim);
-	char *Destport=strtok(NULL,delim);
-	// *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-	// *Dstport points to whatever string came after the delimiter. 
+   
+    char *com[3];
+    int j = 0;
+    com[j] = strtok(srv_msg, " ");
+    while (com[j] != NULL && j < 2) {
+        com[++j] = strtok(NULL, " ");
+    }
 
-	port=atoi(Destport);
-	#ifdef DEBUG 
-		printf("Host %s, and port %d.\n",Desthost,port);
-	#endif
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		printf("Cannot create socket");
-		return 0;
-	}
+    if (com[0] == NULL || com[1] == NULL || com[2] == NULL) { 
+        printf("Invalid operation or operands received.\n");
+        close(sock);
+        return 0;
+    }
 
-	struct sockaddr_in client;
-	client.sin_family = AF_INET;
-	client.sin_port = htons(port);
-	client.sin_addr.s_addr = inet_addr(Desthost);
+    int i1, i2, iresult;
+    double f1, f2, fresult;
 
-	int request = connect(sock, (struct sockaddr *)  &client, sizeof(client));
-	if (request < 0) {
-		return 0;
-	}
+    char command[10];
+    strncpy(command, com[0], sizeof(command) - 1);
+    command[sizeof(command) - 1] = '\0';
 
-	char buffer[1024], srv_msg[1024];
-	char *message;
-	message = "OK\n";
-	if (recv(sock, &buffer, sizeof(buffer), 0) < 0) {
-		return 0;
-	}
-	printf("%s", buffer);
-	if (strcmp(buffer, "TEXT TCP 1.0\n\n") == 0) {
-		if (send(sock, message, strlen(message), 0) < 0) {
-			return 0;
-		}
-	}
-	bzero(srv_msg, sizeof(srv_msg));
-	if (recv(sock, &srv_msg, sizeof(srv_msg), 0) < 0) {
-		return 0;
-	}
-	printf("recieved operation and operands are : %s", srv_msg);
-	int j = 0;
-	char *com[3];
-	com[j] = strtok(srv_msg, " ");
-	while (com[j] != NULL)
-	{
-		com[++j] = strtok(NULL, " ");
-	}
-	int i1, i2, iresult;
-	float f1, f2, fresult;
+    char result[256];
+    if (command[0] == 'f') {
+        f1 = atof(com[1]);
+        f2 = atof(com[2]);
 
-	char command[10];
-	strcpy(command, com[0]);
-	char result[256];
-	if (command[0] == 'f') {
-		f1 = atof(com[1]);
-		f2 = atof(com[2]);
-		if (strcmp(command, "fadd") == 0) {
-			fresult = f1 + f2;
-		}
-		else if (strcmp(command, "fmul") == 0) {
-			fresult = f1 * f2;
-		}
-		else if (strcmp(command, "fsub") == 0) {
-			fresult = f1 - f2;
-		}
-		else if (strcmp(command, "fdiv") == 0) {
-			fresult = f1 / f2;
-		}
-		printf("%s %8.8g %8.8g = %8.8g\n", command, f1, f2, fresult);
-		sprintf(result, "%8.8g\n", fresult);
-		if (send(sock, result, strlen(result), 0) < 0) {
-			return 0;
-		}
+        if (strcmp(command, "fadd") == 0) {
+            fresult = f1 + f2;
+        } else if (strcmp(command, "fmul") == 0) {
+            fresult = f1 * f2;
+        } else if (strcmp(command, "fsub") == 0) {
+            fresult = f1 - f2;
+        } else if (strcmp(command, "fdiv") == 0) {
+            fresult = f1 / f2;
+        } else {
+            printf("Unknown command: %s\n", command);
+            close(sock);
+            return 0;
+        }
 
-	}
-	else {
-		i1 = atoi(com[1]);
-		i2 = atoi(com[2]);
-		if (strcmp(command, "add") == 0) {
-			iresult = i1 + i2;
-		}
-		else if (strcmp(command, "sub") == 0) {
-			iresult = i1 - i2;
-		}
-		else if (strcmp(command, "div") == 0) {
-			iresult = i1 / i2;
-		}
-		else if (strcmp(command, "mul") == 0) {
-			iresult = i1 * i2;
-		}
-		printf("%s %d %d = %d\n", command, i1, i2, iresult);
-		sprintf(result, "%d\n", iresult);
-		if (send(sock, result, strlen(result), 0) < 0) {
-			return 0;
-		}
-	}
-	char reply[256];
-	if (recv(sock, &reply, sizeof(reply), 0) < 0) {
-		return 0;
-	}
-	printf("\n server reply: %s\n", reply);
-	close(sock);
+        printf("%s %8.8g %8.8g\n", command, f1, f2);
+        snprintf(result, sizeof(result), "%8.8g\n", fresult);
+    } else {
+        i1 = atoi(com[1]);
+        i2 = atoi(com[2]);
 
-	return 0;
+        if (strcmp(command, "add") == 0) {
+            iresult = i1 + i2;
+        } else if (strcmp(command, "sub") == 0) {
+            iresult = i1 - i2;
+        } else if (strcmp(command, "div") == 0) {
+            iresult = i1 / i2;
+        } else if (strcmp(command, "mul") == 0) {
+            iresult = i1 * i2;
+        } else {
+            printf("Unknown command: %s\n", command);
+            close(sock);
+            return 0;
+        }
 
+        printf("Assignment: %s %d %d\n", command, i1, i2 );
+        snprintf(result, sizeof(result), "%d\n", iresult);
+    }
 
-  
+    if (send(sock, result, strlen(result), 0) < 0) {
+        perror("Failed to send result");
+        close(sock);
+        return 0;
+    }
+
+    // 
+    bzero(buffer, sizeof(buffer));
+    if (recv(sock, buffer, sizeof(buffer), 0) < 0) {
+        perror("Failed to receive server reply");
+        close(sock);
+        return 0;
+    }
+    printf("Server reply: %s", buffer);
+
+    close(sock);
+    return 0;
 }
--- servermain.cpp
+++ servermain.cpp
@@ -6,148 +6,217 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
+#include <signal.h>
 #include "calcLib.c"
 #include <calcLib.h>
+#include <netdb.h>
+#include <sys/select.h>
 
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
 
+int operation(int cnctn){
+    char rcv_msg[1450];
+    bzero(rcv_msg, sizeof(rcv_msg));
+    strcpy(rcv_msg, "TEXT TCP 1.0\n\n");
+    if (send(cnctn, rcv_msg, strlen(rcv_msg), 0) < 0) {
+        return 0;
+    }
+    bzero(rcv_msg, sizeof(rcv_msg));
+    if (recv(cnctn, &rcv_msg, sizeof(rcv_msg), 0) < 0) {
+        return 0;
+    }
 
-using namespace std;
+    char *ptr;
+    ptr = randomType();
 
+    double f1, f2, fresult;
+    int i1, i2, iresult;
+    char reply[1450];
+    char client_reply[1450];
+    float D;
+
+    if (ptr[0] == 'f') {
+
+        f1 = randomFloat();
+        f2 = randomFloat();
+        bzero(reply, sizeof(reply));
+        sprintf(reply, "%s %8.8g %8.8g\n", ptr, f1, f2);
+        if (strcmp(ptr, "fadd") == 0) {
+            fresult = f1 + f2;
+        }
+        else if (strcmp(ptr, "fsub") == 0) {
+            fresult = f1 - f2;
+        }
+        else if (strcmp(ptr, "fmul") == 0) {
+            fresult = f1 * f2;
+        }
+        else if (strcmp(ptr, "fdiv") == 0) {
+            fresult = f1 / f2;
+        }
+        if (send(cnctn, reply, strlen(reply), 0) < 0) {
+            return 0;
+        }
+        if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
+            return 0;
+        }
+        double result = atof(client_reply);
+        D = abs(result - fresult);
+#ifdef DEBUG
+    printf("Servers result: %8.8g, Clients result: %8.8g \n ", result, fresult);
+#endif
+    }
+    else {
+
+        i1 = randomInt();
+        i2 = randomInt();
+        bzero(reply, sizeof(reply));
+        sprintf(reply, "%s %d %d\n", ptr, i1, i2);
+
+        if (strcmp(ptr, "add") == 0) {
+            iresult = i1 + i2;
+        }
+        else if (strcmp(ptr, "mul") == 0) {
+            iresult = i1 * i2;
+        }
+        else if (strcmp(ptr, "sub") == 0) {
+            iresult = i1 - i2;
+        }
+        else if (strcmp(ptr, "div") == 0) {
+            iresult = i1 / i2;
+        }
+        if (send(cnctn, reply, strlen(reply), 0) < 0) {
+            return 0;
+        }
+        if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
+            return 0;
+        }
+        int result = atof(client_reply);
+        D = abs(result - iresult);
+    }
+
+    const char *pass = "OK\n";
+    const char *fail = "ERROR\n";
+  
+    if (D < 0.0001) {
+        if (send(cnctn, pass, strlen(pass), 0) < 0) {
+            return 0;
+        }
+    }
+    else {
+        if (send(cnctn, fail, strlen(fail), 0) < 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int check_desthost(char *Desthost, struct addrinfo **res){
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(Desthost, NULL, &hints, res) == 0) {
+        if ((*res)->ai_family == AF_INET) {
+            return 1;
+        } else if ((*res)->ai_family == AF_INET6) {
+            return 2;
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]){
-  
-	int sock,request,port;
-	char delim[]=":";
-	char *Desthost=strtok(argv[1],delim);
-	char *Destport=strtok(NULL,delim);
-	// *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-	// *Dstport points to whatever string came after the delimiter. 
-	port=atoi(Destport);
+    int sock, port;
+
+
+    char *input = argv[1];
+    char *last_colon = strrchr(input, ':');
+    if (last_colon == NULL){
+        printf("Accepted Input format host:port\n");
+        return 0;
+    }
+    *last_colon = '\0';  // Replace the last colon with a null terminator
+
+    char *Desthost = input;
+    char *Destport = last_colon + 1;
+
+    struct addrinfo *res;
+    int address_type = check_desthost(Desthost, &res);
+    printf("Address type: %d\n", address_type);
+
+    if (address_type == 0){
+        printf("Invalid IP address\n");
+        return 0;
+    } 
+
+    port = atoi(Destport);
 #ifdef DEBUG  
-	printf("Host %s, and port %d.\n",Desthost,port);
+    printf("Host: %s, Port: %d\n", Desthost, port);
 #endif
+    socklen_t cli_len;
+    cli_len = sizeof(struct sockaddr_in);
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (address_type == 1 || address_type == 2){
+        if (address_type == 1){
+            sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock < 0) {
+                printf("Cannot create socket with IPv4 address\n");
+                return 0;    
+            }
+            struct sockaddr_in server;
+            memcpy(&server, res->ai_addr, sizeof(server));
+            server.sin_port = htons(port);
 
-	struct sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	server.sin_addr.s_addr = inet_addr(Desthost);
+            if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+                printf("Binding failed\n");
+                return 0;
+            }
+        } else if (address_type == 2){
+            sock = socket(AF_INET6, SOCK_STREAM, 0);
+            if (sock < 0) {
+                printf("Cannot create socket with IPv6 address\n");
+                return 0;    
+            }
+            struct sockaddr_in6 server;
+            memcpy(&server, res->ai_addr, sizeof(server));
+            server.sin6_port = htons(port);
 
-	request = bind(sock, (struct sockaddr *) &server, sizeof(server));
-	if (request < 0) {
-		return 0;
-	}
+            if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+                printf("Binding failed\n");
+                return 0;
+            }
+        }
 
-	listen(sock, 10);
-	int cnctn;
-	initCalcLib();
-	while (1) {
-		socklen_t cli_len;
-		cli_len = sizeof(struct sockaddr_in);
-		cnctn = accept(sock, (struct sockaddr *) &server, &cli_len);
-		char rcv_msg[256];
-		bzero(rcv_msg, sizeof(rcv_msg));
-		strcpy(rcv_msg, "TEXT TCP 1.0\n\n");
-		if (send(cnctn, rcv_msg, strlen(rcv_msg), 0) < 0) {
-			return 0;
-		}
-		bzero(rcv_msg, sizeof(rcv_msg));
-		if (recv(cnctn, &rcv_msg, sizeof(rcv_msg), 0) < 0) {
-			return 0;
-		}
+        listen(sock, 5); // Queue up to 5 clients
 
-		char *ptr;
-		ptr = randomType();
+        while (1) {
+            int cnctn = accept(sock, res->ai_addr, &cli_len);
+            if (cnctn < 0) {
+                printf("Accept failed\n");
+                continue;
+            }
 
-		double f1, f2, fresult;
-		int i1, i2, iresult;
-		char reply[256];
-		char srv_result[256];
-		char client_reply[256];
-		float result, D;
+            struct timeval timeout;
+            timeout.tv_sec = 5;
+            timeout.tv_usec = 0;
+            setsockopt(cnctn, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
 
-		if (ptr[0] == 'f') {
+            if (!operation(cnctn)) {
+                char error_msg[] = "ERROR TO\n";
+                send(cnctn, error_msg, strlen(error_msg), 0);
+            }
 
-			f1 = randomFloat();
-			f2 = randomFloat();
-			bzero(reply, sizeof(reply));
-			sprintf(reply, "%s %8.8g %8.8g\n", ptr, f1, f2);
-			if (strcmp(ptr, "fadd") == 0) {
-				fresult = f1 + f2;
-			}
-			else if (strcmp(ptr, "fsub") == 0) {
-				fresult = f1 - f2;
-			}
-			else if (strcmp(ptr, "fmul") == 0) {
-				fresult = f1 * f2;
-			}
-			else if (strcmp(ptr, "fdiv") == 0) {
-				fresult = f1 / f2;
-			}
-			sprintf(srv_result, "%8.8g\n", fresult);
-			if (send(cnctn, reply, strlen(reply), 0) < 0) {
-				return 0;
-			}
-			if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
-				return 0;
-			}
-			result = atof(client_reply);
-			D = abs(result - fresult);
+            close(cnctn);
+        }
 
-		}
-		else {
+        close(sock);
+    }
 
-			i1 = randomInt();
-			i2 = randomInt();
-			bzero(reply, sizeof(reply));
-			sprintf(reply, "%s %d %d\n", ptr, i1, i2);
-
-			if (strcmp(ptr, "add") == 0) {
-				iresult = i1 + i2;
-			}
-			else if (strcmp(ptr, "mul") == 0) {
-				iresult = i1 * i2;
-			}
-			else if (strcmp(ptr, "sub") == 0) {
-				iresult = i1 - i2;
-			}
-			else if (strcmp(ptr, "div") == 0) {
-				iresult = i1 / i2;
-			}
-			sprintf(srv_result, "%d\n", iresult);
-			if (send(cnctn, reply, strlen(reply), 0) < 0) {
-				return 0;
-			}
-			if (recv(cnctn, &client_reply, sizeof(client_reply), 0) < 0) {
-				return 0;
-			}
-			float result, D;
-			result = atof(client_reply);
-			D = abs(result - iresult);
-		}
-
-		char *pass;
-		char *fail;
-		pass = "OK\n";
-		fail = "error\n";
-		if (D < 0.0001) {
-			if (send(cnctn, pass, sizeof(pass), 0) < 0) {
-				return 0;
-			}
-		}
-		else {
-			if (send(cnctn, fail, sizeof(fail), 0) < 0) {
-				return 0;
-			}
-		}
-		close(cnctn);
-	}
-
-	close(sock);
-
-
+    freeaddrinfo(res);
+    return 0;
 }
